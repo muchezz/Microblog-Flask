20d4b494c73cf09e76b65ba215085f928bf74ad5
Muchezz <muchezz@outlook.com>
2020-07-15 11:46:15 +0300

    Email Support

--- app/forms.py
+++ app/forms.py
@@ -55,6 +55,12 @@ class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
     submit = SubmitField('Request Password Reset')
 
+class ResetPasswordForm(FlaskForm):
+    password= PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators= [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[DataRequired()])
     submit = SubmitField('Submit')
