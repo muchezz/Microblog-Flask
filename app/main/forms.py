3d26f558927e31b243e39e7feb2acb15507aa8fb
Muchezz <muchezz@outlook.com>
2020-07-29 23:50:06 +0300

    Full-Text Search

--- app/main/forms.py
+++ app/main/forms.py
@@ -22,6 +22,16 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError(_('Please use a different username.'))
 
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
+
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
